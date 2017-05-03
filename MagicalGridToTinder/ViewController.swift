3841888578fe6054bf066c8b8b6de826e706baf7
蒋轩哲 <jgcm@live.cn>
2017-05-03 19:05:17 +0800

    Full screen rect

--- MagicalGridToTinder/ViewController.swift
+++ MagicalGridToTinder/ViewController.swift
@@ -16,15 +16,24 @@ class ViewController: UIViewController {
         let numViewPerRow = 15
         let width = view.frame.width / CGFloat(numViewPerRow)
         
-        for i in 0..<numViewPerRow {
-            let view = UIView()
-            view.backgroundColor = randomColor()
-            view.frame = CGRect(x: CGFloat(i) * width, y: 0, width: width, height: width)
-            self.view.addSubview(view)
+        for j in 0...30 {
+            for i in 0..<numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 1 / UIScreen.main.scale
+                cellView.layer.borderColor = UIColor.black.cgColor
+                self.view.addSubview(cellView)
+            }
         }
         
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
         
-        
+    }
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        print(location)
     }
     
     fileprivate func randomColor() -> UIColor {
